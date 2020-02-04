2ae7c8f2083e35baa11747a7ff7635ed0514aaaf
cresten <crestenn@gmail.com>
2020-02-03 17:38:37 -0800

    Working djikstras

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ struct Arena<'a> {
     parents: HashMap<&'a String, &'a String>,
     nodes: HashMap<&'a String, &'a Node>,
     edges: HashMap<&'a String, &'a Edge<'a>>,
-    costs: HashMap<&'a String, u64>,
+    costs: HashMap<&'a String, f64>,
     processed: Vec<&'a Node>,
     start: &'a Node,
     end: &'a Node,
@@ -16,8 +16,8 @@ struct Arena<'a> {
 impl<'a> Arena<'a> {
     fn new(start: &'a Node, end: &'a Node) -> Arena<'a> {
         let mut costs = HashMap::new();
-        costs.insert(&start.id, 0);
-        costs.insert(&end.id, std::u64::MAX);
+        costs.insert(&start.id, 0.0);
+        costs.insert(&end.id, std::f64::INFINITY);
         Arena {
             parents: HashMap::new(),
             nodes: HashMap::new(),
@@ -59,14 +59,14 @@ impl<'a> Arena<'a> {
         self.processed.push(node);
     }
 
-    fn get_cost(&self, node: &Node) -> &u64 {
+    fn get_cost(&self, node: &Node) -> &f64 {
         match self.costs.get(&node.id) {
             Some(cost) => cost,
-            None => &std::u64::MAX,
+            None => &std::f64::MAX,
         }
     }
 
-    fn get_weight(&self, node_one: &Node, node_two: &Node) -> u64 {
+    fn get_weight(&self, node_one: &Node, node_two: &Node) -> f64 {
         self.edges
             .iter()
             .map(|(_, edge)| edge)
@@ -86,17 +86,15 @@ impl<'a> Arena<'a> {
                 let node_one_cost = self.get_cost(node_one);
                 let node_two_cost = self.get_cost(node_two);
 
-                node_one_cost.cmp(node_two_cost)
+                node_one_cost.partial_cmp(node_two_cost).unwrap()
             })
     }
 
-    fn djikstra(mut self) {
-        let node = self.find_lowest_cost_node().unwrap();
-        let mut done = false;
-
-        while !done {
-            let cost = self.get_cost(node);
+    fn djikstra(mut self) -> String {
+        let mut optional = self.find_lowest_cost_node();
+        while let Some(node) = optional {
             self.find_neighbors(&node).iter().for_each(|neighbor| {
+                let cost = self.get_cost(node);
                 let neighbor_cost = self.get_weight(&node, &neighbor);
                 let new_cost = cost + neighbor_cost;
                 if self.get_cost(neighbor) > &new_cost {
@@ -105,11 +103,22 @@ impl<'a> Arena<'a> {
                 }
             });
 
-            //self.processed.push(node);
-            done = true;
+            self.processed.push(node);
+            optional = self.find_lowest_cost_node();
         }
-        println!("Costs: {:?}", self.costs);
-        // vec![&node]
+
+        let mut path = vec![&self.end.id];
+
+        let mut optional = self.parents.get(path.last().unwrap());
+        while let Some(child) = optional {
+            path.push(child);
+            optional = self.parents.get(path.last().unwrap());
+        }
+
+        path.reverse();
+
+        println!("path: {:?}", path);
+        path.join(" -> ")
     }
 }
 
@@ -119,6 +128,9 @@ struct Node {
 }
 
 impl Node {
+    fn new_with_id(id: String) -> Node {
+        Node { id: id }
+    }
     fn new() -> Node {
         Node {
             id: Uuid::new_v4().to_string(),
@@ -129,13 +141,13 @@ impl Node {
 #[derive(Debug)]
 struct Edge<'a> {
     id: String,
-    weight: u64,
+    weight: f64,
     end_node: &'a Node,
     start_node: &'a Node,
 }
 
 impl<'a> Edge<'a> {
-    fn new(weight: u64, start_node: &'a Node, end_node: &'a Node) -> Edge<'a> {
+    fn new(weight: f64, start_node: &'a Node, end_node: &'a Node) -> Edge<'a> {
         Edge {
             id: Uuid::new_v4().to_string(),
             weight: weight,
@@ -156,11 +168,11 @@ mod tests {
         let three = Node::new();
         let four = Node::new();
         let not_a_neighbor = Node::new();
-        let start_one = Edge::new(0, &start, &one);
-        let start_two = Edge::new(0, &start, &two);
-        let start_three = Edge::new(0, &start, &three);
-        let start_four = Edge::new(0, &start, &four);
-        let four_not = Edge::new(0, &four, &not_a_neighbor);
+        let start_one = Edge::new(0.0, &start, &one);
+        let start_two = Edge::new(0.0, &start, &two);
+        let start_three = Edge::new(0.0, &start, &three);
+        let start_four = Edge::new(0.0, &start, &four);
+        let four_not = Edge::new(0.0, &four, &not_a_neighbor);
         let nodes = [&one, &two, &three, &four, &not_a_neighbor];
         let edges = [&start_one, &start_two, &start_three, &start_four, &four_not];
         let arena = Arena::new(&start, &four)
@@ -188,11 +200,11 @@ mod tests {
         let three = Node::new();
         let four = Node::new();
         let not_a_neighbor = Node::new();
-        let start_one = Edge::new(0, &start, &one);
-        let start_two = Edge::new(0, &start, &two);
-        let start_three = Edge::new(0, &start, &three);
-        let start_four = Edge::new(0, &start, &four);
-        let four_not = Edge::new(0, &four, &not_a_neighbor);
+        let start_one = Edge::new(0.0, &start, &one);
+        let start_two = Edge::new(0.0, &start, &two);
+        let start_three = Edge::new(0.0, &start, &three);
+        let start_four = Edge::new(0.0, &start, &four);
+        let four_not = Edge::new(0.0, &four, &not_a_neighbor);
         let nodes = [&start, &one, &two, &three, &four, &not_a_neighbor];
         let edges = [&start_one, &start_two, &start_three, &start_four, &four_not];
         let arena = Arena::new(&start, &not_a_neighbor)
@@ -203,33 +215,37 @@ mod tests {
 
     #[test]
     fn djikstra_test() {
-        let book = Node::new();
-        let poster = Node::new();
-        let lp = Node::new();
-        let drums = Node::new();
-        let bassguitar = Node::new();
-        let piano = Node::new();
-        let start_poster = Edge::new(0, &book, &poster);
-        let book_lp = Edge::new(5, &book, &lp);
-        let poster_bassguitar = Edge::new(30, &poster, &bassguitar);
-        let lp_drums = Edge::new(20, &lp, &drums);
-        let bassguitar_piano = Edge::new(20, &bassguitar, &piano);
-        let drums_piano = Edge::new(10, &drums, &piano);
+        let book = Node::new_with_id("book".to_string());
+        let poster = Node::new_with_id("poster".to_string());
+        let lp = Node::new_with_id("lp".to_string());
+        let drums = Node::new_with_id("drums".to_string());
+        let bassguitar = Node::new_with_id("bassguitar".to_string());
+        let piano = Node::new_with_id("piano".to_string());
+        let start_poster = Edge::new(0.0, &book, &poster);
+        let book_lp = Edge::new(5.0, &book, &lp);
+        let poster_bassguitar = Edge::new(30.0, &poster, &bassguitar);
+        let poster_drums = Edge::new(35.0, &poster, &drums);
+        let lp_drums = Edge::new(20.0, &lp, &drums);
+        let lp_bassguitar = Edge::new(15.0, &lp, &bassguitar);
+        let bassguitar_piano = Edge::new(20.0, &bassguitar, &piano);
+        let drums_piano = Edge::new(10.0, &drums, &piano);
 
         let nodes = [&book, &poster, &lp, &drums, &bassguitar, &piano];
         let edges = [
             &start_poster,
             &book_lp,
             &poster_bassguitar,
+            &poster_drums,
             &lp_drums,
+            &lp_bassguitar,
             &bassguitar_piano,
             &drums_piano,
         ];
 
-        let arena = Arena::new(&poster, &piano)
+        let arena = Arena::new(&book, &piano)
             .add_nodes(&nodes)
             .add_edges(&edges);
 
-        println!("{:?}", arena.djikstra());
+        assert_eq!("", arena.djikstra())
     }
 }
